ae4e1faf66a7089680d28db3f0be2d41397e4070
Sone <sthaya@gmail.com>
2020-06-30 16:44:53 -0700

    created starter files for backend, setup knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,41 @@
-// Update with your config settings.
+require("dotenv").config();
+
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
 
 module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/items.db3'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+  },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+  },
+  seeds: {
+    directory: "./data/seeds",
+  },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: pgConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 
 };
